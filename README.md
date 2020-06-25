032e5f04bc85602c05f12daf697788b9fca308c4
Vinicios Batista <sonicgyn@hotmail.com>
2020-06-24 21:29:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## reactjs-concepts
+# About the project
+Just an application to train what I have learned so far in ReactJS!
---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import api from '../src/services/api';
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] =  useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories',{
+      id: "123",
+      url: "https://github.com/josepholiveira",
+      title: "Desafio ReactJS",
+      techs: ["React", "Node.js"],
+    });
+    
+    setRepositories([ ...repositories, response.data]);
+
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+    const newRepositories = repositories.filter(
+      repository => repository.id !== id
+    )
+    setRepositories(newRepositories);
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository =>
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+          </li>  
+        )}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
